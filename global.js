d1f4e6a2b56b7d7bca82f1141c2574780cebb8ef
joellegg <joellegg@bellsouth.net>
2017-03-02 14:34:23 -0600

    removed console.log(process) to clean up the file

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-console.log(process);
+// console.log(process);
 // with objects, you don't need the , to get to the correct spots. You can just call them by their name.
 let {node, v8} = process.versions;
 console.log(`Node.js version: v${node}\nV8 version: v${v8}`);
