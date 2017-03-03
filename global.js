13e53a16734e37c42acbd9b871b0bf55c7da17f3
joellegg <joellegg@bellsouth.net>
2017-03-03 09:31:45 -0600

    commented alternative methods

--- global.js
+++ global.js
@@ -2,4 +2,7 @@
 // console.log(process);
 // with objects, you don't need the , to get to the correct spots. You can just call them by their name.
 let {node, v8} = process.versions;
+// can also do let {versions: {node, v8}} = process;
+// OR you can rename them {versions: {node: nodetastic, v8: camaro}} = process;
+// THEN console.log(`Node.js version: v${nodetastic}\nv8 version: v${camaro}`);
 console.log(`Node.js version: v${node}\nV8 version: v${v8}`);
